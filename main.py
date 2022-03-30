d8adfe53aa593cf9854aa500c551551981304ef4
Richcard Larose <lrichcard@gmail.com>
2022-03-29 18:57:45 -0700

    adding ssl and modify the structure

--- main.py
+++ main.py
@@ -2,23 +2,19 @@
 #pip install smtplib-> to install smtplib
 
 import smtplib
+from email.message import EmailMessage
+email1 = "pipokolo2@gmail.com"
+msg = EmailMessage()
+msg['Subject'] = "Money subject"
+msg['From'] =  email1
+msg['To'] = 'lrichcard@gmail.com'
+msg.set_content('This is the body of my message\n with ssl')
+password = input(str("please enter the password"))
 
-# addin ssl
-# with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    # smtp.ehlo()
-    # smtp.starttls()
-    # smtp.ehlo()
-    email1 = "pipokolo2@gmail.com"
-    password = input(str("please enter the password"))
-
     try:
         smtp.login(email1, password)
-        subject = "Money subject"
-        body = "This is the body of my message\n with ssl"
-
-        msg = f'Subject: {subject}\n\n{body}'
-        smtp.sendmail(email1, 'lrichcard@gmail.com', msg)
         print("Message sent..")
     except Exception:
         print("sorry somethin went wrong")
